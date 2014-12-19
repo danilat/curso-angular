22597ef0a3cf729afb0ef9da37440affa2d05fa9
danilat <danilat83@gmail.com>
2014-12-19 20:07:30 +0100

    data binding

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@
         $scope.name = 'puto amo';
         $scope.categories = ['HTML5','JavaScript','CSS','Games'];
         $scope.bookmarks = [
-            {id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
-            {id:2,name:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
-            {id:3,name:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
-            {id:4,name:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
-            {id:5,name:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
+            {id:1,title:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
+            {id:2,title:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
+            {id:3,title:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
+            {id:4,title:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
+            {id:5,title:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
         ];
 
         $scope.currentCategory = 'JavaScript';
@@ -24,8 +24,34 @@
         $scope.isCurrentCategory = function(category){
             return $scope.currentCategory === category;
         }
+
+        $scope.showWindow = function(bookmark){
+            $scope.bookmarkForm.$setPristine();
+            $scope.bookmarkForm.$setUntouched();
+
+            bookmark = bookmark || {category:$scope.currentCategory,url:''};
+            $scope.bookmark = bookmark;
+            $('#bookmarkModal').modal('show');
+        }
+
         $scope.save = function(bookmark){
-            console.log(bookmark);
+            if($scope.bookmarkForm.$valid){
+                if(!bookmark.id){
+                    var record = angular.copy(bookmark);
+                    record.id = $scope.bookmarks.length;
+                    $scope.bookmarks.push(record);
+                }
+                $('#bookmarkModal').modal('hide');
+            }
+        }
+
+        $scope.remove = function(id){
+            for(var i=0,len=$scope.bookmarks.length;i<len;i++){
+                if($scope.bookmarks[i].id === id){
+                    $scope.bookmarks.splice(i,1);
+                    break;
+                }
+            }
         }
     });
 })();
