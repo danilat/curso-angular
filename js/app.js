12fbe8f4d8c6c0d42e8613c8e520156fa0d9623e
danilat <danilat83@gmail.com>
2014-12-23 11:27:08 +0100

    now the category directive works well with controller

--- js/app.js
+++ js/app.js
@@ -21,15 +21,35 @@
         });
     })
 
-    .directive('bootstrapSelect',function(){
+    .directive('bootstrapSelect',function($parse){
         return {
-            require: 'ngModel',
-            link: function (scope, element, attrs, ngModel) {
-                $(element).selectpicker();
+            link: function (scope, element, attrs) {
                 var collection = attrs.bootstrapSelect,
                 valueProperty = attrs.selectValue,
                 labelProperty = attrs.selectLabel,
-                model = attrs.ngModel;
+                model = attrs.selectModel,
+                getter = $parse(model),
+                setter = getter.assign;
+
+                $(element).selectpicker();
+
+                scope.$watch(model,function(data){
+                    if(angular.isObject(data)){
+                        $(element).selectpicker('val',data[valueProperty]);
+                    }
+                });
+
+                $(element).change(function(){
+                    var col = scope[collection],
+                        val = $(element).val();
+
+                    for(var i=0,len=col.length;i<len;i++){
+                        if(val == col[i][valueProperty]){
+                            setter(scope,col[i]);
+                            break;
+                        }
+                    }
+                });
 
                 scope.$watch(collection,function(data){
                     if(data){
